a1ce44a8b8ba8c0b30b111bfde6d2ec86e6e4461
agnaldo26 <agnaldosilva9704@gmail.com>
2021-12-31 20:05:35 -0300

    made the main functionality of the back-end

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
+import 'express-async-errors';
 import express from 'express';
+import { createConnection } from 'typeorm';
+import routes from './routes';
+import { globalErrors } from './middlewares/globalErrors';
 
-const app = express();
-const PORT = 3333;
+createConnection()
+  .then(connection => {
+    const app = express();
+    const PORT = 3333;
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+    app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
+    app.use(routes);
+
+    app.use(globalErrors);
+
+    app.listen(PORT, () => {
+      console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.log('Unable to connect to the database', error);
+  });
