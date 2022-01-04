acc693b2f17f3b1913d85232d8df96a63eb1e528
agnaldo26 <agnaldosilva9704@gmail.com>
2022-01-03 22:03:09 -0300

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import 'express-async-errors';
 import express from 'express';
 import { createConnection } from 'typeorm';
-import routes from './routes';
+import cors from 'cors';
+
 import { globalErrors } from './middlewares/globalErrors';
+import routes from './routes';
 
 createConnection()
   .then(connection => {
     const app = express();
     const PORT = 3333;
 
+    app.use(cors());
     app.use(express.json());
-
     app.use(routes);
 
     app.use(globalErrors);
